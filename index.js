ae03fefa4d16911f588fea28681becf9bec8ff67
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-01-11 19:02:52 +0900

    検索ボタンを押したらaccept/rejectボタンを表示

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ $(function (){
     });
     */
 
+    $("#btn-search").click(function() {
+        var btn_act = '<input type="button" value="accept" id="btn-accept">'
+        $("body").append(btn_act);
+        var btn_rjc = '<input type="button" value="reject" id="btn-reject">'
+        $("body").append(btn_rjc);
+    });
+
 });
