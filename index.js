7f4659f92c35dc38eeb0b1bd60259d2242d2dfdc
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-01-13 15:56:23 +0900

    検索ボタンを押したら、ランダムで候補者が表示されるようにした

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ $(function (){
     // IPC通信を行うために、レンダラープロセス側では ipcRenderer というモジュールを require する
     const { ipcRenderer } = require("electron");
 
+    //NeDBの設定
+    var Datastore = require('nedb');
+    var db = new Datastore({ 
+        filename: 'data/member.db',
+        autoload: true
+    });
+
     $("#btn-search").click(function() {
         // "hello" という文字列と123という整数を送信
         ipcRenderer.send("test", 1,2); 
+
+        db.findOne({}, function(err, docs){
+            $("#rlt-name").html(docs.name);
+            $("#rlt-date").html(docs.date);
+        });
         
         var btn_act = '<input type="button" value="accept" id="btn-accept">'
         $("body").append(btn_act);
