e84e6eaa01eb173f168e73e855f24f41f08a4697
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-02-01 22:15:08 +0900

    name列の変更とDBのupdateを同期

--- table.js
+++ table.js
@@ -24,6 +24,7 @@ $(function (){
             });
 
             function successFunc(){
+                db.update({name:cellValue}, {$set: {name: editor.value}});
                 success(editor.value);
             }
             editor.addEventListener("blur", successFunc);
