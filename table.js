c29f7ef08213d9f88eca3067ebdf65af270be08f
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-02-01 19:17:43 +0900

    コードを簡潔化

--- table.js
+++ table.js
@@ -9,13 +9,8 @@ $(function (){
     });
 
     db.find({}, function(err, docs){
-        var data_length = docs.length;
-        for(var i=0;i<data_length;i++){
-            var tag = "<tr><td>" + docs[i].name + "</td><td>" + docs[i].date + "</td></tr>";
-            $("tbody").append(tag);
-        }
-        //ipcRenderer.send("test", docs); console.logの代わり
-        var table = new Tabulator("#result_list", {
+        var table = new Tabulator("#example-table", {
+            data:docs,
             autoColumns:true,
         });
     });
@@ -33,21 +28,7 @@ $(function (){
         };
         db.insert(doc);
 
-        var tag = "<tr><td>" + m_name + "</td><td>" + today + "</td></tr>";
-        $("tbody").append(tag);
-
         $("#form-input").val("");
-    });
-
-    //tabulator導入
-    var tabledata = [
-        {id:1, name:"Billy Bob", age:12, gender:"male", height:95, col:"red", dob:"14/05/2010"},
-        {id:2, name:"Jenny Jane", age:42, gender:"female", height:142, col:"blue", dob:"30/07/1954"},
-        {id:3, name:"Steve McAlistaire", age:35, gender:"male", height:176, col:"green", dob:"04/11/1982"},
-    ];
-    var table = new Tabulator("#example-table", {
-        data:tabledata,
-        autoColumns:true,
     });
 
 });
