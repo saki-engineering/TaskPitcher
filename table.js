bb303f594123fb4a3108f7f4faeef5d0c1bad302
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-02-01 19:01:40 +0900

    tabulatorを導入してテストテーブルを表示

--- table.js
+++ table.js
@@ -35,4 +35,15 @@ $(function (){
         $("#form-input").val("");
     });
 
+    //tabulator導入
+    var tabledata = [
+        {id:1, name:"Billy Bob", age:12, gender:"male", height:95, col:"red", dob:"14/05/2010"},
+        {id:2, name:"Jenny Jane", age:42, gender:"female", height:142, col:"blue", dob:"30/07/1954"},
+        {id:3, name:"Steve McAlistaire", age:35, gender:"male", height:176, col:"green", dob:"04/11/1982"},
+    ];
+    var table = new Tabulator("#example-table", {
+        data:tabledata,
+        autoColumns:true,
+    });
+
 });
