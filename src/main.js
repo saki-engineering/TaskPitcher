d87018f80d28c45694723c0e518a9c57a129bdc4
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-02-05 00:53:44 +0900

    htmlファイルをtemplatesに分離

--- src/main.js
+++ src/main.js
@@ -69,7 +69,7 @@ app.on('ready', function(){
   mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
     nodeIntegration: true
   }});
-  mainWindow.loadURL('file://' + __dirname + '/home.html');
+  mainWindow.loadURL('file://' + __dirname + '/templates/home.html');
 
   //メニューバー設置
   const menu = Menu.buildFromTemplate(template);
