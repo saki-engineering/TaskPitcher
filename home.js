877211fb6c371267a9aa2b094fef4aa22d1d9c58
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-02-04 22:40:46 +0900

    home.jsにエラー処理を追加

--- home.js
+++ home.js
@@ -11,31 +11,44 @@ $(function (){
 
     const moment = require('moment')
 
-    //候補者をランダムに表示→accpet処理s
+    //候補者をランダムに表示→accpet処理
     $("#btn-search").click(function() {
         // "hello" という文字列と123という整数を送信
         //ipcRenderer.send("test", 1); 
 
+        //選ばれた候補者をaccept処理する
         function select_conf(c_name,c_period,c_date){
-            var dialog = c_period + "期" + c_name + "さん(最終割り振り: " + c_date + "が選ばれました。\nこの人に割り振りますか？";
+            var title = c_name + "さんが選ばれました"
+            var dialog = c_period + "期" + c_name + "さん(最終割り振り: " + c_date + ")に仕事を割り振りますか？";
             Swal.fire({
-                title: '候補者を選びました',
+                title: title,
                 text: dialog,
                 icon: 'info',
                 showCancelButton: true,
                 confirmButtonText: 'Yes'
             }).then((result) => {
                 if (result.value) {
-                    db.update({name:c_name, date:c_date, period:c_period}, {$set: {date: moment().format('YYYY-MM-DD')}});
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'SUCCESS!',
-                        text: '処理が終わりました',
-                    })
+                    db.update({name:c_name, date:c_date, period:c_period}, {$set: {date: moment().format('YYYY-MM-DD')}}, function(err, newDoc){
+                        if (err !== null){
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'データベースの更新に失敗しました',
+                                text: err,
+                            })
+                        }
+                        else{
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'SUCCESS!',
+                                text: c_name + 'さんの最終割り振り日を更新しました',
+                            })
+                        }
+                    });
                 }
             })
         }
 
+        //候補者を選ぶ
         db.find({active:1, date:{$lte: moment().subtract(1,'M').format('YYYY-MM-DD')}}, function(err, docs){
             if(docs.length>0){
                 var random = Math.floor( Math.random() * docs.length );
@@ -50,8 +63,8 @@ $(function (){
                     else{
                         Swal.fire({
                             icon: 'error',
-                            title: 'ERROR',
-                            text: '選べる候補者が一人もいません',
+                            title: '選べる候補者が一人もいません',
+                            text: 'メンバーを追加するか、Member ListからメンバーをActiveにしてください。',
                         })
                     }
                 });
