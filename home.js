9ff75c22ae311df1798663620a610d7fc8b12ebb
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-02-04 23:25:01 +0900

    候補者を選ぶときに、備考欄も一緒に更新できるようにした

--- home.js
+++ home.js
@@ -17,18 +17,21 @@ $(function (){
         //ipcRenderer.send("test", 1); 
 
         //選ばれた候補者をaccept処理する
-        function select_conf(c_name,c_period,c_date){
+        function select_conf(c_name,c_period,c_date,c_remarks){
             var title = c_name + "さんが選ばれました"
             var dialog = c_period + "期" + c_name + "さん(最終割り振り: " + c_date + ")に仕事を割り振りますか？";
             Swal.fire({
                 title: title,
                 text: dialog,
                 icon: 'info',
+                input: 'text',
+                inputPlaceholder: '備考欄の更新',
+                inputValue: c_remarks,
                 showCancelButton: true,
                 confirmButtonText: 'Yes'
             }).then((result) => {
                 if (result.value) {
-                    db.update({name:c_name, date:c_date, period:c_period}, {$set: {date: moment().format('YYYY-MM-DD')}}, function(err, newDoc){
+                    db.update({name:c_name, date:c_date, period:c_period}, {$set: {date: moment().format('YYYY-MM-DD'), remarks: result.value}}, function(err, newDoc){
                         if (err !== null){
                             Swal.fire({
                                 icon: 'error',
@@ -40,7 +43,7 @@ $(function (){
                             Swal.fire({
                                 icon: 'success',
                                 title: 'SUCCESS!',
-                                text: c_name + 'さんの最終割り振り日を更新しました',
+                                text: c_name + 'さんの最終割り振り日と備考欄を更新しました',
                             })
                         }
                     });
@@ -52,13 +55,13 @@ $(function (){
         db.find({active:1, date:{$lte: moment().subtract(1,'M').format('YYYY-MM-DD')}}, function(err, docs){
             if(docs.length>0){
                 var random = Math.floor( Math.random() * docs.length );
-                select_conf(docs[random].name,docs[random].period,docs[random].date);
+                select_conf(docs[random].name,docs[random].period,docs[random].date,docs[random].remarks);
             }
             else{
                 db.find({active:1}, function(err, docsum){
                     if(docsum.length>0){
                         var random = Math.floor( Math.random() * docsum.length );
-                        select_conf(docsum[random].name,docsum[random].period,docsum[random].date);
+                        select_conf(docsum[random].name,docsum[random].period,docsum[random].date,docsum[random].remarks);
                     }
                     else{
                         Swal.fire({
