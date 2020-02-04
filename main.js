ae90bdc48544e90870672ec90135c3b12780bb80
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-02-04 23:43:43 +0900

    アプリケーションメニューの設定を記述

--- main.js
+++ main.js
@@ -9,8 +9,45 @@ const app = electron.app;
 // ウィンドウを作成するモジュール
 const BrowserWindow = electron.BrowserWindow;
 
+//アプリケーションメニュー
+const Menu = electron.Menu
+
 // メインウィンドウはGCされないようにグローバル宣言
 let mainWindow;
+
+//メニューバー内容
+let template = [{
+  label: 'TaskPitcher',
+  submenu: [{
+    label: 'ReadMe',
+    click: function(){
+      open('https://github.com/saki-engineering/TaskPitcher', '_blank');
+    }
+  }, {
+    type: 'separator'
+  }, {
+    label: 'コピー',
+    accelerator: 'CmdOrCtrl+C',
+    role: 'copy'
+  }]
+}, {
+  label: '表示',
+  submenu: [{
+    label: '全画面表示切り替える',
+    accelerator: (function() {
+      if (process.platform === 'darwin') {
+        return 'Ctrl+Command+F'
+      } else {
+        return 'F11'
+      }
+    })(),
+    click: function(item, focusedWindow) {
+      if (focusedWindow) {
+        focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+      }
+    }
+  }]
+}]
 
 // Electronの初期化完了後に実行
 app.on('ready', function(){
@@ -19,6 +56,10 @@ app.on('ready', function(){
     nodeIntegration: true
   }});
   mainWindow.loadURL('file://' + __dirname + '/home.html');
+
+  //メニューバー設置
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
   // ウィンドウが閉じられたらアプリも終了
   mainWindow.on('closed', function(){
