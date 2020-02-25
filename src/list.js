988161908ce552b73dec4fb4d93616e7b390c973
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-02-25 14:17:42 +0900

    エラー分の追加

--- src/list.js
+++ src/list.js
@@ -12,164 +12,196 @@ $(function (){
     const fs = window.fs;
     const dialog = window.dialog;
 
-    db.find({}, function(err, docs){
-        //editorの設定
-        var nameEditor = function(cell, onRendered, success, cancel){
-            //create and style input
-            var cellValue = cell.getValue();
-            editor = document.createElement("input");
-            editor.style.padding = "4px";
-            editor.style.width = "100%";
-            editor.style.boxSizing = "border-box";
-            editor.value = cellValue;
+    //editorの設定
+    var nameEditor = function(cell, onRendered, success, cancel){
+        //create and style input
+        var cellValue = cell.getValue();
+        editor = document.createElement("input");
+        editor.style.padding = "4px";
+        editor.style.width = "100%";
+        editor.style.boxSizing = "border-box";
+        editor.value = cellValue;
 
-            onRendered(function(){
-                editor.focus();
-                editor.style.height = "100%";
-            });
-
-            function successFunc(){
-                db.update({_id:cell.getData()._id}, {$set: {name: editor.value}});
-                success(editor.value);
-            }
-            editor.addEventListener("blur", successFunc);
-        
-            return editor;
-        };
-
-        var periodEditor = function(cell, onRendered, success, cancel){
-            //create and style input
-            var cellValue = cell.getValue();
-            editor = document.createElement("input");
-            editor.style.padding = "4px";
-            editor.style.width = "100%";
-            editor.style.boxSizing = "border-box";
-            editor.value = cellValue;
-
-            onRendered(function(){
-                editor.focus();
-                editor.style.height = "100%";
-            });
-
-            function successFunc(){
-                var peri = Number(editor.value);
-                if(peri>0　&& Number.isInteger(peri)){
-                    db.update({_id:cell.getData()._id}, {$set: {period: editor.value}});
-                    success(editor.value);
-                }
-                else{
-                    alert("不正な数字です");
-                    success(cellValue);
-                }
-            }
-
-            editor.addEventListener("blur",successFunc);
-        
-            return editor;
-        };
-
-        var dateEditor = function(cell, onRendered, success, cancel){
-            //create and style input
-            var cellValue = cell.getValue();
-            editor = document.createElement("input");
-            editor.style.padding = "4px";
-            editor.style.width = "100%";
-            editor.style.boxSizing = "border-box";
-            editor.value = cellValue;
-
-            onRendered(function(){
-                editor.focus();
-                editor.style.height = "100%";
-            });
-
-            function successFunc(){
-                if(moment(editor.value).isValid()){
-                    db.update({_id:cell.getData()._id}, {$set: {date: moment(editor.value).format('YYYY-MM-DD')}});
-                    success(moment(editor.value).format('YYYY-MM-DD'));
-                }
-                else{
-                    alert("不正な日付です");
-                    success(cellValue);
-                }
-            }
-
-            editor.addEventListener("blur",successFunc);
-        
-            return editor;
-        };
-        
-        var remarksEditor = function(cell, onRendered, success, cancel){
-            //create and style input
-            var cellValue = cell.getValue();
-            editor = document.createElement("input");
-            editor.style.padding = "4px";
-            editor.style.width = "100%";
-            editor.style.boxSizing = "border-box";
-            editor.value = cellValue;
-
-            onRendered(function(){
-                editor.focus();
-                editor.style.height = "100%";
-            });
-
-            function successFunc(){
-                db.update({_id:cell.getData()._id}, {$set: {remarks: editor.value}});
-                success(editor.value);
-            }
-            editor.addEventListener("blur", successFunc);
-        
-            return editor;
-        };
-
-        var activeEditor = function(cell, onRendered, success, cancel){
-            //create and style input
-            var cellValue = cell.getValue();
-            editor = document.createElement("input");
-            editor.setAttribute('type', 'checkbox');
-            if(cellValue == 1) editor.checked = true;
-            else editor.checked = false;
-
-            function successFunc(){
-                if(editor.checked){
-                    db.update({_id:cell.getData()._id}, {$set: {active: 1}});
-                    success(1);
-                }
-                else{
-                    db.update({_id:cell.getData()._id}, {$set: {active: 0}});
-                    success(0);
-                }
-            }
-            editor.addEventListener("blur", successFunc);
-        
-            return editor;
-        };
-
-        //表を表示
-        var table = new Tabulator("#result-table", {
-            data:docs,
-            layout:"fitColumns",
-            columns:[
-                {formatter:"rowSelection", titleFormatter:"rowSelection", align:"center", width:10, headerSort:false, cellClick:function(e, cell){
-                    cell.getRow().toggleSelect();
-                }},
-                {title:"Name", field:"name", editor:nameEditor,},
-                {title:"Period", field:"period", editor:periodEditor,},
-                {title:"Last-date", field:"date", editor:dateEditor,},
-                {title:"Remarks", field:"remarks", editor:remarksEditor,},
-                {title:"Active", field:"active", formatter:"tickCross", editor:activeEditor,headerFilter:"tickCross", headerFilterParams:{"tristate":true},headerFilterEmptyCheck:function(value){return value === null}},
-            ],
+        onRendered(function(){
+            editor.focus();
+            editor.style.height = "100%";
         });
 
-        //データ削除ボタンの設定
-        $("#del-rows").click(function(){
+        function successFunc(){
+            db.update({_id:cell.getData()._id}, {$set: {name: editor.value}});
+            success(editor.value);
+        }
+        editor.addEventListener("blur", successFunc);
+    
+        return editor;
+    };
+
+    var periodEditor = function(cell, onRendered, success, cancel){
+        //create and style input
+        var cellValue = cell.getValue();
+        editor = document.createElement("input");
+        editor.style.padding = "4px";
+        editor.style.width = "100%";
+        editor.style.boxSizing = "border-box";
+        editor.value = cellValue;
+
+        onRendered(function(){
+            editor.focus();
+            editor.style.height = "100%";
+        });
+
+        function successFunc(){
+            var peri = Number(editor.value);
+            if(peri>0　&& Number.isInteger(peri)){
+                db.update({_id:cell.getData()._id}, {$set: {period: editor.value}});
+                success(editor.value);
+            }
+            else{
+                alert("不正な数字です");
+                success(cellValue);
+            }
+        }
+
+        editor.addEventListener("blur",successFunc);
+    
+        return editor;
+    };
+
+    var dateEditor = function(cell, onRendered, success, cancel){
+        //create and style input
+        var cellValue = cell.getValue();
+        editor = document.createElement("input");
+        editor.style.padding = "4px";
+        editor.style.width = "100%";
+        editor.style.boxSizing = "border-box";
+        editor.value = cellValue;
+
+        onRendered(function(){
+            editor.focus();
+            editor.style.height = "100%";
+        });
+
+        function successFunc(){
+            if(moment(editor.value).isValid()){
+                db.update({_id:cell.getData()._id}, {$set: {date: moment(editor.value).format('YYYY-MM-DD')}});
+                success(moment(editor.value).format('YYYY-MM-DD'));
+            }
+            else{
+                alert("不正な日付です");
+                success(cellValue);
+            }
+        }
+
+        editor.addEventListener("blur",successFunc);
+    
+        return editor;
+    };
+    
+    var remarksEditor = function(cell, onRendered, success, cancel){
+        //create and style input
+        var cellValue = cell.getValue();
+        editor = document.createElement("input");
+        editor.style.padding = "4px";
+        editor.style.width = "100%";
+        editor.style.boxSizing = "border-box";
+        editor.value = cellValue;
+
+        onRendered(function(){
+            editor.focus();
+            editor.style.height = "100%";
+        });
+
+        function successFunc(){
+            db.update({_id:cell.getData()._id}, {$set: {remarks: editor.value}});
+            success(editor.value);
+        }
+        editor.addEventListener("blur", successFunc);
+    
+        return editor;
+    };
+
+    var activeEditor = function(cell, onRendered, success, cancel){
+        //create and style input
+        var cellValue = cell.getValue();
+        editor = document.createElement("input");
+        editor.setAttribute('type', 'checkbox');
+        if(cellValue == 1) editor.checked = true;
+        else editor.checked = false;
+
+        function successFunc(){
+            if(editor.checked){
+                db.update({_id:cell.getData()._id}, {$set: {active: 1}});
+                success(1);
+            }
+            else{
+                db.update({_id:cell.getData()._id}, {$set: {active: 0}});
+                success(0);
+            }
+        }
+        editor.addEventListener("blur", successFunc);
+    
+        return editor;
+    };
+
+    let table;
+
+    //表を表示
+    db.find({}, function(err, docs){
+        if (err !== null){
             Swal.fire({
-                title: '確認',
-                text: '選択したデータを本当に削除しますか？',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonText: 'Yes'
-            }).then((result) => {
-                if (result.value) {
+                icon: 'error',
+                title: 'データの取得に失敗しました',
+                text: err,
+            })
+        }
+        else{
+            table = new Tabulator("#result-table", {
+                data:docs,
+                layout:"fitColumns",
+                columns:[
+                    {formatter:"rowSelection", titleFormatter:"rowSelection", align:"center", width:10, headerSort:false, cellClick:function(e, cell){
+                        cell.getRow().toggleSelect();
+                    }},
+                    {title:"Name", field:"name", editor:nameEditor,},
+                    {title:"Period", field:"period", editor:periodEditor,},
+                    {title:"Last-date", field:"date", editor:dateEditor,},
+                    {title:"Remarks", field:"remarks", editor:remarksEditor,},
+                    {title:"Active", field:"active", formatter:"tickCross", editor:activeEditor,headerFilter:"tickCross", headerFilterParams:{"tristate":true},headerFilterEmptyCheck:function(value){return value === null}},
+                ],
+            });
+        }
+    });
+
+    //フィルターの設置
+    function updateFilter(){
+        table.setFilter($("#filter-field").val(), $("#filter-type").val(), $("#filter-value").val());
+    }
+    $("#filter-field, #filter-type").change(updateFilter);
+    $("#filter-value").keyup(updateFilter);
+    $("#filter-clear").click(function(){
+        $("#filter-field").val("");
+        $("#filter-type").val("=");
+        $("#filter-value").val("");
+        table.clearFilter();
+    });
+
+    //データ削除ボタンの設定
+    $("#del-rows").click(function(){
+        Swal.fire({
+            title: '確認',
+            text: '選択したデータを本当に削除しますか？',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes'
+        }).then((result) => {
+            if (result.dismiss!=="cancel") {
+                if(typeof(table)==="undefined"){
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'tableが取得できませんでした',
+                    });
+                }
+                else{
                     var selected_data = table.getSelectedData();
                     var fail = [];
 
@@ -197,34 +229,60 @@ $(function (){
                         });
                     }
                 }
-            })
-        });
+            }
+        })
+    });
 
-        //DBの中身をcsvに出力するボタン
-        $("#output-csv").click(function(){
-            db.find({}, function(err, docs){
-                //書き出すデータの作成
-                var input = [];
-                for(var i=0;i<docs.length;i++){
-                    var obj = {
-                        id: docs[i]._id,
-                        name: docs[i].name,
-                        date: docs[i].date,
-                        period: docs[i].period,
-                        remarks: docs[i].remarks
-                    };
-                    input.push(obj);
-                }
-                //ファイル選択ダイアログの表示
-                var saved_filepath = dialog.showSaveDialogSync(null, {
-                    title: '保存',
-                    defaultPath: app.getPath('home'),
-                    filters: [
-                        {name: 'CSVファイル', extensions: ['csv']},
-                    ]
+    //DBの中身をcsvに出力するボタン
+    $("#output-csv").click(function(){
+        //書き出すデータの作成
+        var input = [];
+        var output_err;
+        db.find({}, function(err, docs){
+            output_err = err;
+            for(var i=0;i<docs.length;i++){
+                var obj = {
+                    id: docs[i]._id,
+                    name: docs[i].name,
+                    date: docs[i].date,
+                    period: docs[i].period,
+                    remarks: docs[i].remarks
+                };
+                input.push(obj);
+            }
+        });
+        //ファイル選択ダイアログの表示
+        var saved_filepath = dialog.showSaveDialogSync(null, {
+            title: '保存',
+            defaultPath: app.getPath('home'),
+            filters: [
+                {name: 'CSVファイル', extensions: ['csv']},
+            ]
+        });
+        if(saved_filepath){
+            if(output_err){
+                Swal.fire({
+                    icon: 'error',
+                    title: '表データ取得に失敗しました',
+                    text: output_err
                 });
-                if(saved_filepath){
-                    stringify(input,{header: true},(err, output) => {
+            }
+            else if(input.length == 0){
+                Swal.fire({
+                    icon: 'info',
+                    title: '表にデータがありません',
+                });
+            }
+            else{
+                stringify(input,{header: true},(err, output) => {
+                    if(err){
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'csvファイルの生成に失敗しました',
+                            text: err
+                        });
+                    }
+                    else{
                         fs.writeFile(saved_filepath, output , (err) => {
                             if(err){
                                 Swal.fire({
@@ -242,24 +300,10 @@ $(function (){
                                 });
                             }
                         });
-                    });
-                }
-            });
-        });
-
-        //フィルターの設置
-        function updateFilter(){
-            table.setFilter($("#filter-field").val(), $("#filter-type").val(), $("#filter-value").val());
+                    }
+                });
+            }
         }
-        $("#filter-field, #filter-type").change(updateFilter);
-        $("#filter-value").keyup(updateFilter);
-        $("#filter-clear").click(function(){
-            $("#filter-field").val("");
-            $("#filter-type").val("=");
-            $("#filter-value").val("");
-            table.clearFilter();
-        });
-
-    });
+    });    
 
 });
